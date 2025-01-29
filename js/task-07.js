91a3a050c50103b302e68e3a814594aa762502fe
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2025-01-29 08:12:33 +0100

    jobber med pæren
    jeg la til en melding som jeg tidligere hadde slettet

--- js/task-07.js
+++ js/task-07.js
@@ -3,11 +3,14 @@ let isFlashlightOn = false;
 function toggleFlashlight() {
   const body = document.body;
   const flashlight = document.getElementById("flashlight");
+  const message = document.getElementById("message");
 
   if (isFlashlightOn) {
     body.classList.remove("flashlight-on");
+    message.textContent = "Flashlight is off";
   } else {
     body.classList.add("flashlight-on");
+    message.textContent = "Flashlight is on";
   }
 
   isFlashlightOn = !isFlashlightOn;
